ebd61c6e1c05e038c71f7ca6fc96b025c144cee1
Scott Corgan <scottcorgan@gmail.com>
2013-07-08 18:24:54 -0700

    Test update and refactor.

--- test/test.js
+++ test/test.js
@@ -5,25 +5,13 @@ var bufferJsonStream = require('../index');
 test('Buffering', function (t) {
   t.plan(1);
   
-  var stream = through().pause();
-  
-  stream.queue({
-    some: 'object'
-  });
-  
-  stream.queue({
-    someOther: 'object'
-  });
-  
-  //
+  var stream = through().pause().queue({ some: 'object' }).queue({ someOther: 'object' });
   stream.pipe(bufferJsonStream()).on('data', function (data) {
     
     t.deepEqual(data, [
       { some: 'object' },
       { someOther: 'object' }
     ], 'Buffers stream of JSON objects');
-    
-    t.end();
   });
   
   stream.resume().end();
